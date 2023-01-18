a1d4f07c78843d581283978760e810682b5abffb
Marvin Berrio <marvinccp@gmail.com>
2023-01-18 16:50:17 +0100

    Home Styles 20%

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import BaseLayout from "./Layouts/Layout";
 import styles from '../styles/Index.module.css'
+import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <>
-      <BaseLayout title={'Home'} />
+      <BaseLayout title={"Home"} />
       <main className={styles.index_container}>
-        <h1>Eyy</h1>
-        <h3>Aquí encontrarás recetas</h3>
+        <h1>Find the perfect recipe</h1>
+        <div className={styles.image_container}>
+          <Link href={'/recipes/'}>
+            <Image
+              className={styles.image}
+              src={"/images/chef2.png"}
+              alt={"recipes-logo-app"}
+              width={800}
+              height={800}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+              priority={true}
+            />
+          </Link>
+        </div>
       </main>
     </>
   );
